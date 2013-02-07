c63364061391b695b2e1e6de4bf1a2bcef22865a
Jason Knight <jason@jason-knight-martin.com>
2013-02-07 12:08:36 -0500

    gemfile and routes initialized for salor-cart

--- salor-retail/config/routes.rb
+++ salor-retail/config/routes.rb
@@ -170,5 +170,8 @@ SalorRetail::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  if defined? SalorCart == 'constant'
+    mount SalorCart::Engine => "/cart"
+  end
   root :to => "home#index"
 end
